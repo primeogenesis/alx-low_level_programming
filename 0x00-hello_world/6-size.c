39b57326e1b26027493ea76302bd998266f47927
primeogenesis <primemailin@gmail.com>
2022-10-19 15:06:08 -0700

    correction on assignment 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ int main(void)
 	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
 	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long int));
-	printf("Size of a long long int: %ul byte(s)\n", (unsigned int)sizeof(long long int));
+	printf("Size of a long long int: %u byte(s)\n",
+			(unsigned int)sizeof(long long int));
 	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
 	return (0);
 
