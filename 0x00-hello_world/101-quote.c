75b6731316dd6435485c0a830c60174213186a30
primeogenesis <primemailin@gmail.com>
2022-10-21 14:59:46 -0700

    finished

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program to print to stderr
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
