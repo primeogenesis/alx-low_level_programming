cda8f7cf6eaba70f71adfbc0e80e0d3f4e237e92
primeogenesis <primemailin@gmail.com>
2022-10-25 23:31:55 -0700

    xrcis 4 don

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints 'a'-'z' execept 'q' and 'e'
+*
+* Return: 0 (Always)
+*/
+
+int main(void)
+{
+	int i;
+
+	/* ascii char 'a' is 97 which follow serially to char 'z' which 122 */
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
