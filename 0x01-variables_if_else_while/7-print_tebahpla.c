758857ba9de2c6d1c1b7f4116b738ac724091f13
primeogenesis <primemailin@gmail.com>
2022-10-26 00:03:14 -0700

    eond htiw 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints 'a'-'z' in reverse
+*
+* Return: 0 (Always)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
