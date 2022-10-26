0092d491723f82dedb50e0e7d1da22969fe3fb84
primeogenesis <primemailin@gmail.com>
2022-10-25 23:13:30 -0700

    done exercise 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints 'a'-'z' then 'A' - 'Z' and then newline
+*
+* Return: 0 (Always)
+*/
+
+int main(void)
+{
+	int i;
+
+	/* ascii char 'a' is 97 which follow serially to char 'z' which 122 */
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+/*
+*	printf(" >%d to %d diff %d\n >%d to %d diff %d\n"
+*	, 'a', 'z', 'a' - 'z', 'A', 'Z', 'A' - 'Z'); // bg
+*/
+
+}
