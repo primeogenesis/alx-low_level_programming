52d3e1035966fb12f8df69b1582d9ffef79df73a
primeogenesis <primemailin@gmail.com>
2022-10-25 21:22:21 -0700

    try again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,34 @@
 #define NONNUM 'g'
 
 /**
+ *print_eval - stdout evaluation
+ *@n2: takes random number
+ *@lastdigit2: takes last digit
+ * Return: void
+ */
+
+void print_eval(int n2, int long lastdigit2)
+	{
+		if (lastdigit2 == 0)
+		{
+			printf("Last digit of %d is %ld and is 0\n", n2, lastdigit2);
+		}
+		else if (lastdigit2 < 6)
+		{
+			printf("Last digit of %d is %ld and is less than 6 and not 0\n",
+					n2, lastdigit2);
+		}
+		else
+		{
+			printf("Last digit of %d is %ld and is greater than 5\n",
+					n2, lastdigit2);
+		}
+
+		return;
+
+	}
+
+/**
 * main - label random number
 *
 * Return: o (always)
@@ -29,19 +57,13 @@ int main(void)
 		i--;
 		lastdigit = strtol(str + i, NULL, 10);
 
-		if (lastdigit == 0)
+		if (n < 0)
 		{
-			printf("Last digit of %d is %ld and is 0\n", n, lastdigit);
+			lastdigit /= -1;
 		}
-		else if (lastdigit < 6)
-		{
-			printf("Last digit of %d is %ld and is less than 6 and not 0\n",
-				n, lastdigit);
-		}
-		else
-		{
-			printf("Last digit of %d is %ld and is greater than 5\n", n, lastdigit);
-		}
+
+		print_eval(n, lastdigit);
+
 		/*printf("lastdigit of %d is: %ld\n", n, lastdigit); //test */
 
 
