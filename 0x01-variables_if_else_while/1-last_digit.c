4d3e949c3f9211b638506917c4a74396336f6180
primeogenesis <primemailin@gmail.com>
2022-10-25 18:58:39 -0700

    edited 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,21 @@ int main(void)
 		}
 		i--;
 		lastdigit = strtol(str + i, NULL, 10);
-		/*printf("lastdigit of %d is: %ld\n", n, lastdigit);*/
+
+		if (lastdigit == 0)
+		{
+			printf("Last digit of %d is %ld and is 0\n", n, lastdigit);
+		}
+		else if (lastdigit < 6)
+		{
+			printf("Last digit of %d is %ld and is less than 6 and not 0\n",
+				n, lastdigit);
+		}
+		else
+		{
+			printf("Last digit of %d is %ld and is greater than 5\n", n, lastdigit);
+		}
+		/*printf("lastdigit of %d is: %ld\n", n, lastdigit); //test */
 
 
 
