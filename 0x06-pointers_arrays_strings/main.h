9396024c9bba88e1082a7b16b623560992a3dc3c
primeogenesis <primemailin@gmail.com>
2022-10-30 12:45:57 -0700

    strncpy function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -43,4 +43,23 @@ char *_strncat(char *dest, char *src, int n);
  */
 int _putchar(char c);
 
+/**
+*       _strncpy - copies string
+*       using n as a delimiter for data
+*       from string @src
+*
+*       @dest: the string to be copied over
+*       the string should have sufficient space to
+*       contain source @src
+*
+*       @src: the string copied to @dest
+*
+*       @n: number of char to copy.
+*
+*       Return: @dest would be copied to.
+*
+*/
+
+char *_strncpy(char *dest, char *src, int n);
+
 #endif /* MAIN_H_INCLUDED */
