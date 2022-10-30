d75c97f13cbbe32a74c0ade60ed1e76d3fbff8bf
primeogenesis <primemailin@gmail.com>
2022-10-30 13:44:24 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -23,22 +23,24 @@ char *_strncpy(char *dest, char *src, int n)
 {
 
 	int i = 0;
+	int end_idx = 0;
 
 	if (dest == NULL || src == NULL || n <= 0)
 		return (dest);
 
-/*
-*	while (dest[end_idx] != '\0')
-*	{
-*		end_idx++;
-*	}
-*/
+
+	while (dest[end_idx] != '\0')
+	{
+		end_idx++;
+	}
+
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0'; /* terminates
+
+	dest[end_idx] = '\0'; /* terminates
 *dest with null without this statement.
 * reason unknown
 */
